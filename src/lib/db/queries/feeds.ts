592949c327eb6635b81783ba79b4649bc3915ae7
Teodor Koynov <teodor.koynov2005@gmail.com>
2026-01-06 01:14:19 +0200

    aggregate loop

--- src/lib/db/queries/feeds.ts
+++ src/lib/db/queries/feeds.ts
@@ -1,6 +1,6 @@
+import {eq, sql} from "drizzle-orm";
 import {db} from "../index";
 import {feeds} from "../schema";
-import {eq} from "drizzle-orm";
 
 export async function createFeed(feedName: string, url: string, userId: string) {
     const [result] = await db.insert(feeds).values({ name: feedName, url, userId }).returning();
@@ -15,5 +15,24 @@ export async function getFeedByURL(url: string) {
     return await db.query.feeds.findFirst({
         where: eq(feeds.url, url)
     })
+}
 
+export async function markFeedFetched(feedId: string) {
+    const [result] = await db
+        .update(feeds)
+        .set({
+            lastFetchedAt: new Date(),
+        })
+        .where(eq(feeds.id, feedId))
+        .returning();
+    return result;
+}
+
+export async function getNextFeedToFetch() {
+    const [result] = await db
+        .select()
+        .from(feeds)
+        .orderBy(sql`${feeds.lastFetchedAt} asc nulls first`)
+        .limit(1);
+    return result;
 }
